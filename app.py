e44f4c3506c7cb3310777ec3848200697c99aa0c
Lab- AI Lab 310 DS <sankalpppppp@gmail.com>
2026-02-18 16:01:58 +0530

    Initial ommit

--- app.py
+++ app.py
@@ -1,1 +1,44 @@
-print("Hello from GitHub Actions CI!")
+# Function definitions
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
+
+
+# Menu
+print("Simple Calculator")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+# User input
+choice = int(input("Enter choice (1-4): "))
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Operation selection
+if choice == 1:
+    print("Result =", add(num1, num2))
+
+elif choice == 2:
+    print("Result =", subtract(num1, num2))
+
+elif choice == 3:
+    print("Result =", multiply(num1, num2))
+
+elif choice == 4:
+    print("Result =", divide(num1, num2))
+
+else:
+    print("Invalid choice")
